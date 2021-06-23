69fcee0097df1ffc8da259097e6d1cefa4f5b9f8
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-23 20:15:34 +0200

    Update welmenu.js

--- src/welmenu.js
+++ src/welmenu.js
@@ -2,17 +2,12 @@ const welmenu = (prefix, pushname) => {
     return `
 *Comandos De Bienvenida 👋*
 
-*PARA ACTIVAR LA BIENVENIDA*
-
-- ${prefix}welcome 1
-
-*PARA DESACTIVAR LA BIENVENIDA*
-
-- ${prefix}welcome 0
+╠ ○ PARA ACTIVAR LA BIENVENIDA*
+  ${prefix}welcome 1
+╠ ○ PARA DESACTIVAR LA BIENVENIDA*
+  ${prefix}welcome 0
 
 _Para activar este comando el bot necesita tener admin_
-
-by Lalelilolu
 `
 
 }
