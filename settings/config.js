0aafece97a8b8e3075fb39d618d0c8be6eb93960
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-24 05:42:05 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
         botName: 'ShanBot',
-        ownerName: 'Shanduy',
+        ownerName: 'Lalelilolu',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
