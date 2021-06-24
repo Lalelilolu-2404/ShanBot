505c0f538babbdf4d2700ac3afb9203ab8829c3b
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-24 03:45:26 +0200

    Create bana.js

---
+++ src/bana.js
@@ -0,0 +1,40 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes, pinches otakus*
+
+- A
+- Ayy
+- Acm1pt
+- A mimir
+- Brr
+- Brr2
+- Dime onichan
+- Feliz jueves
+- Gambare
+- Help
+- Hentai
+- Iluminati
+- La toca :3
+- Lokita
+- Me gimes?
+- Me vengo
+- Mi reina
+- Mujer
+- Nya
+- Pack
+- Pasa pack
+- Petardo
+- Pete
+- Quien es tu sempai 
+- Sapee
+- Setso
+- Sparta
+- Te amo botsito
+- Yamete 
+- Yamete kudasai
+
+_Ojito escribe tal como esta_
+by Lalelilolu`
+
+}
+
+exports.bana = bana
