54137530cb7cc3d70c804127ad172be601eec933
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-24 06:53:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -447,9 +447,9 @@ async function starts() {
 		case 'menuadmin':
 		client.sendMessage(from, menuadmin(prefix, sender), text, {quoted: mek})
 		break
-		case 'nsfwmenu':
+		/**case 'nsfwmenu':
 		client.sendMessage(from, nsfwmenu(prefix, sender), text, {quoted: mek})
-		break
+		break**/
 		case 'desmenu':
 		client.sendMessage(from, desmenu(prefix, sender), text, {quoted: mek})
 		break
