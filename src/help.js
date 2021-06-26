4c17b99f4df3c7119d941a491775f6e291f9706c
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-26 11:46:12 +0200

    Update help.js

--- src/help.js
+++ src/help.js
@@ -2,7 +2,7 @@ const help = (prefix) => {
 	return `
 	
 *⌜《Lalelilolu》\◔,◡◔,/ ت♡⌟*  
-client.sendMessage(from, `.assets/menuimg.jpg`, image, {quoted: mek, caption: ':v'})
+(from, `.assets/menuimg.jpg`
 
 ◉ *INFORMACION*
   ╠ ○ Comando: ⌜ ${prefix} ⌟
