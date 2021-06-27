75c34a25b99ba6dd56131744868631b7a81c0f40
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-27 21:00:47 +0200

    Create packsito.js

---
+++ src/packsito.js
@@ -0,0 +1,11 @@
+const packsito = (prefix, pushname) => {
+    return `*Comandos para ver packsitos 🥵*
+*Packsito ✅*
+- ${prefix}dolf
+
+_El bot nesecita admin y tener activado los PACKSITO_\n _Digita_\n ${prefix}*packsito 1*
+by Lalelilolu`
+
+}
+
+exports.packsito = packsito
