e673733fa7755fba15de90f37d14178a95c8d09d
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-28 12:31:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -439,7 +439,7 @@ async function starts() {
 			/**const none = fs.readFileSync('./assets/menuimg.jpeg');			
 			client.sendMessage(from, none, image, {quoted:mek, caption : help(prefix, sender)})
 			/**client.sendMessage(from, help(prefix), text)**/**/
-					
+			loli = fs.readFileSync(`./mp3/fernan.mp3`)		
 			wew = fs.readFileSync(`./src/menuimg.jpeg`)
 			zain = help(prefix, sender)
 			ShanBot.sendMessage(from, wew, image, {quoted: {key:{fromMe: false, participant: `0@s.whatsapp.net`, ...(from? {remoteJid: "status@broadcast"} : {})}, message: {"imageMessage":{"url": https://mmg.whatsapp.net/d/f/At0x7Zdlvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc",  "mimetype": "image/jpeg","caption":"Lalelilolu*","fileSha256":"+la+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=","fileLenght":"28777","height":1080,"width":1079,"mediaKey":"vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=","fileEncSha256":"sR9D2RS5JSifw49HeBADgul23fWDz1aZu4faWG/CyRY=","directPath":"/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69","mediaKeyTimestamp":"1610993486","jpegThumbnail":fs.readFileSync('src/menuimg.jpeg')}}},caption: zain, pushname, prefix, getLevelingXp, getLevelingLevel, sender, role})
