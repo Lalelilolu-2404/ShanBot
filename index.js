1700ecdb7f5605e65509a0e281535cf1be1c9c25
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-29 14:06:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -438,7 +438,7 @@ async function starts() {
             pato.sendMessage(from, teks, image, {thumbnail:fs.readFileSync('./assets/menuimg.jpeg'),quoted:mek,caption:yes})
         }				
 
-	        const fakestatus = (teks) => {
+	const fakestatus = (teks) => {
             pato.sendMessage(from, teks, text, {
                 quoted: {
                     key: {
@@ -516,39 +516,6 @@ message: {
             })
         }
 
-
- const fakestatus = (teks) => {
-            pato.sendMessage(from, teks, text, {
-                quoted: {
-                    key: {
-                        fromMe: false,
-                        participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
-                    },
-                    message: {
-                        "imageMessage": {
-                            "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc",
-                            "mimetype": "image/jpeg",
-                            "caption": `Holi cosita ^-^${pushname}`,
-                            "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=",
-                            "fileLength": "28777",
-                            "height": 1080,
-                            "width": 1079,
-                            "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=",
-                            "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=",
-                            "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69",
-                            "mediaKeyTimestamp": "1610993486",
-                            "jpegThumbnail": fs.readFileSync('./assets/menuimg.jpeg'),
-                            "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw=="
-                        }
-                    },
-                    contextInfo: {
-                      "forwardingScore": 999, "isForwarded": true
-                    }
-                }
-            })
-        }
-		
- 
 
 const faketokoforwaded = (teks) => {
 	anu = {
