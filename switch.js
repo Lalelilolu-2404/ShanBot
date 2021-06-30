37f4e121f565ca24ba18cd3eb34911cadee5cfdb
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-30 09:27:15 +0200

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,12 @@
+const switch = (prefix, pushname) => {
+    return `◪ *Comandos de switch*
+    │
+    ├─ ❏ ${prefix}interact 1/0
+    ├─ ❏ ${prefix}welcome 0/1
+    ├─ ❏ ${prefix}nsfw 0/1
+    ├─ ❏ ${prefix}packsito 0/1
+    └─ ❏ ${prefix}stick 0/1
+    `
+}
+
+exports.switch = switch
