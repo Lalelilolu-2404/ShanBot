798f2c4664c9929289e39746e34f91a14d71ed43
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-30 09:44:45 +0200

    Create switchmenu.js

---
+++ src/switchmenu.js
@@ -0,0 +1,16 @@
+
+const switchmenu = (prefix, pushname) => {
+    return `*Comandos switch xd*
+    
+- ${prefix}interact 1/0
+- ${prefix}nsfw 0/1
+- ${prefix}stick 0/1
+- ${prefix}packsito 0/1
+- ${prefix}welcome 0/1
+
+_El bot nesecita admin y tener activado los SWITCH_\n
+by Lalelilolu`
+
+}
+
+exports.switchwmenu = switchmenu
