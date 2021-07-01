6f10d1e8e4537dd05d0df63e7263164ff9231c81
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-07-01 10:40:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -859,9 +859,6 @@ mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
 	
 	ppimg = await client.getProfilePicture(`${mentioned[0].split('@')[0]}@c.us`)
 	let Mh = await getBuffer(ppimg)
-	
-	console.log(anu)
-	num = anu.participants[0]
 					
 	//client.sendMessage(from, buff, image, {quoted: mek, caption: 'Uwu'})
 	teks = `「 *Uwu cosita :3* 」${nombre}`
@@ -907,7 +904,7 @@ mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
 	let Mh = await getBuffer(ppimg)
 						
 	//client.sendMessage(from, buff, image, {quoted: mek, caption: 'Uwu'})
-	teks = `「 *Uwu cosita :3* ${nombre.mentioned[0]} 」\n ${mentioned[0].replace('@s.whatsapp.net', '')}\n @${mentioned[0]}\n@${mentioned[0].split('@')[0]} `
+	teks = `「 *Uwu cosita :3* ${mentioned} 」\n ${mentioned.replace('@s.whatsapp.net', '')}\n@${mentioned[0].split('@')[0]} `
 	
             client.sendMessage(from, teks, text, {
                 quoted: {
