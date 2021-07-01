4ff5b7451c921c9c3f492da57bdff2cb633c9f06
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-07-01 13:13:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -930,7 +930,7 @@ mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
                         "imageMessage": {
                             "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc",
                             "mimetype": "image/jpeg",
-                            "caption": `Cosita ^-^\n@${mentioned[0].split('@')[0]}`
+                            "caption": `Cosita ^-^\n@${mentioned[0].split('@')[0]}`,
 			   // "caption": `「 *Uwu cosita :3* 」`,
                             "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=",
                             "fileLength": "28777",
