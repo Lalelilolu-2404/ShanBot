32514c0e22efb69ce2b66c0dbb82f4da0f18dc90
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-08-13 08:35:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -453,7 +453,7 @@ ${demote}`
 			
 			const conts = mek.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
 			//let pushname = client.contacts[sender] != undefined ? client.contacts[sender].vname || client.contacts[sender].notify: undefined
-			let pushname = mek.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
+			const pushname = mek.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || 'U_U'
 			  
 
 	        //FUNCION DE LEVEL
@@ -1904,10 +1904,10 @@ if (args.length < 1) return reply(`Incorrecto \nComando : ${prefix}daftar Nombre
 	var nombre = reg.split("|")[0];	
 	user.push(sender)	
 	fs.writeFileSync('./database/json/user.json', JSON.stringify(user))
-const nameUser = `${pushname}`
-const umurUser = `${sender}`
+//const nameUser = `${pushname}`
+//const umurUser = `${sender}`
 //const serialUser = `${actu.sender}`
-veri = sender
+//veri = sender
 	
 /**client.sendMessage(from, `\`\`\`REGISTRADO ✅\`\`\`\n\n\`\`\`
 	Hora EC: ${time}\`\`\`\n\`\`\`
@@ -1937,7 +1937,7 @@ veri = sender
  // 	╠≽️ *User*\`\: *${nameUser}*
 //	╠≽️ *Status* : *${serialUser}  
 		
-//	teks =  `「*${pushname}*」`
+	teks =  `「*${pushname}*」`
 //	`⊱ღ꧁ ${pushname} ꧂ღ⊱ 
             client.sendMessage(from, teks, text, {
                 quoted: {
@@ -3351,7 +3351,7 @@ const Fer =  ["Ban", "Funao", "Love", "Ya se durmieron", "Te me calmas", "Ta fue
 if(isGroupAdmins || isMe || isLoli || isMeuf){
 	if(!isOwner){
 	for (let i = 0; i < Fer.length; i++){
-		if (budy.includes(`${Fer[i]}`)){
+		if (budy.includes(`${Fer[i]}`) && budy.length == Fer[i].length){
 			none = fs.readFileSync(`./src/stickers/${Fer[i]}.webp`)
 			client.sendMessage(from, none, sticker, {quoted: mek})	
 		}			
@@ -3363,13 +3363,16 @@ if(isGroupAdmins || isMe || isLoli || isMeuf){
 /////////////////////////////////////
 const Puw =  ["Nani", "Lolxd", "F", "Amm", "Pichula", "Si xd", "Haha no"]	
 if (!isGroup) return reply(mess.only.group)
+if(!isGroupAdmins)
+if(!isOwner){
 	for (let i = 0; i < Puw.length; i++){
-		if (budy.includes(`${Puw[i]}`)){
+		if (budy.includes(`${Puw[i]}`) && budy.length == Puw[i].length){
 			none = fs.readFileSync(`./src/stickers2/${Puw[i]}.webp`)
 			client.sendMessage(from, none, sticker)	
 		}			
 		
-	}				
+	}
+}				
 		
 ////////////María
 		
