1063a1490587ec15b4e4b2270e283714f48f7a1a
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-11 01:59:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -430,7 +430,8 @@ var menu =`Nightcore  -  Rock mix
   ╠ ${prefix}tts ⌜Code⌟ + ⌜Text⌟
   ╠ ${prefix}play + ⌜Text⌟
   ╠ ${prefix}play2 + ⌜Text⌟
-  ╠ ${prefix}ytmp4 + ⌜Link⌟ `	
+  ╠ ${prefix}ytmp4 + ⌜Link⌟ 
+  `	
 faketokoforwaded(menu)							
 break	
 		
@@ -470,7 +471,7 @@ case 'spam':
 				spst = "Haha no"
 				Noperro = fs.readFileSync(`./src/stickers2/${spst}.webp`)
 				client.sendMessage(from, Noperro, MessageType.sticker, {quoted: mek})
-				argz[1] = 10
+				argz[1] = 5
 			}
                 for (let i = 0; i < argz[1]; i++){
                 client.sendMessage(from, argz[0], MessageType.text)
