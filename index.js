8f54aa3298cfeb6765734230b5578e95c2c51adf
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-10 20:46:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,48 +88,6 @@ const {
 ///
 const antilink = JSON.parse(fs.readFileSync('./src/antilink.json'))
 /******INICIO DE FUNCIONES ENTRADA******/
-
-function addMetadata(packname, author) {	
-	if (!packname) packname = 'Anna se la come :v'; if (!author) author = '⛧⸸⁶Death⁹†حب♡ت';	
-	author = author.replace(/[^a-zA-Z0-9]/g, '');	
-	let name = `${author}_${packname}`
-	if (fs.existsSync(`./${name}.exif`)) return `./${name}.exif`
-	const json = {	
-		"sticker-pack-name": packname,
-		"sticker-pack-publisher": author,
-	}
-	const littleEndian = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])	
-	const bytes = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]	
-
-	let len = JSON.stringify(json).length	
-	let last	
-
-	if (len > 256) {	
-		len = len - 256	
-		bytes.unshift(0x01)	
-	} else {	
-		bytes.unshift(0x00)	
-	}	
-
-	if (len < 16) {	
-		last = len.toString(16)	
-		last = "0" + len	
-	} else {	
-		last = len.toString(16)	
-	}	
-
-	const buf2 = Buffer.from(last, "hex")	
-	const buf3 = Buffer.from(bytes)	
-	const buf4 = Buffer.from(JSON.stringify(json))	
-
-	const buffer = Buffer.concat([littleEndian, buf2, buf3, buf4])	
-
-	fs.writeFile(`./${name}.exif`, buffer, (err) => {	
-		return `./${name}.exif`	
-	})	
-
-} 
-	
 function kyun(seconds){
   function pad(s){
     return (s < 10 ? '0' : '') + s;
@@ -664,11 +622,11 @@ break
 								fs.unlinkSync(ran)
 							})
 						})
-							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-						}
-						break
+.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+}
+break
 					
 			            case 'toimg':
 				    client.updatePresence(from, Presence.composing)
