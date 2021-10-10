a197c78b12ee64ef3d86aa710e1c8599d34f6181
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-10 19:08:46 +0200

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,32 @@
+const help = (prefix) => {
+	return` 	
+⌜⛧⸸⁶Death⁹†حب♡ت⌟  
+Nightcore  -  Rock mix  
+01:52 ━━━●───── 03:08
+     ⇆ㅤㅤ ◁ㅤ ❚❚ㅤ ▷ㅤ ㅤ↻﻿
+                  ılıılıılıılıılıılı
+⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫
+◉ *INFORMACION*
+  ╠ ○ Comando: ⌜ ${prefix} ⌟
+  ╠ ○ Creador: Lalelilolu
+   
+◉ *SPAM :3*
+  ╠ ○ ${prefix}swt ⌜@Tag⌟ | # de veces | ⌜Texte⌟ 
+  ╠ Spam al privado, max 50 #. Solo admins
+  ╠ ○ ${prefix}spam ⌜Texte⌟ | # de veces 
+  ╚ Spam grupo, max 30. Todos 
+
+◉ *CREAR STICKERS*
+  ╠ ○ ${prefix}s / ticker
+
+◉ *AUDIO*
+  ╠ ○ ${prefix}idioma
+  ╠ Code para ${prefix}tts
+  ╠ ○ ${prefix}tts ⌜Code⌟ + ⌜Texto⌟
+  ╚ 
+
+⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫⩫  
+⌜⛧⸸⁶Death⁹†حب♡ت⌟ 
+`
+}
+exports.help = help
