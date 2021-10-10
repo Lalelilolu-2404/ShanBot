d51b35ccb859a40d147591dfff1fbb03ec102b82
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-10 20:47:48 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Lalelilolu',
+        botName: 'Anna gil',
+        ownerName: '⛧⸸⁶Death⁹†حب♡ت',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
